b08133aa7d2d1bc252ced03a81ec991bb76d0adb
namxy-admin <nm.ymuc@gmail.com>
2025-12-07 15:29:53 +0900

    DBに初期データを格納（Users：適当なユーザ情報、Subjects：国数英）

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('Subjects')->insert([
+            ['subject' => 'Japanese',
+            'created_at' => now()],
+            ['subject' => 'Math',
+            'created_at' => now()],
+            ['subject' => 'English',
+            'created_at' => now()]
+        ]);
         // 国語、数学、英語を追加
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('Users')->insert([
+            ['over_name' => '田中',
+            'under_name' => '太郎',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'User1@mailaddress.com',
+            'sex' => '1',
+            'birth_day' => '2000-01-01',
+            'role' => '1',
+            'password' => bcrypt('password1'),
+            'created_at' => now(),
+            'updated_at' => now()]
+        ]);
     }
 }
